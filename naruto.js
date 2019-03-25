49f6790812da378867d75a6cd61c2770fc1328cc
Rajat <khanna.rjt@gmail.com>
2019-03-25 05:32:58 +0000

    Remove gitignored files

--- naruto.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-module.exports = {
-  pickJutsu: function() {
-    jutsuList = fs.readFileSync('jutsu.txt', 'utf-8').split('\n');
-    return jutsuList[Math.floor(Math.random()*jutsuList.length)];
-  }
-}
