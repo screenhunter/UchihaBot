04f7d0f84d1d69b814441dd3ac2b06ebb57df412
Rajat <khanna.rjt@gmail.com>
2019-03-26 01:20:27 +0000

    Fixed authentication

--- bot.js
+++ bot.js
@@ -6,20 +6,25 @@ client.on('ready', () => {
   console.log('Logged in as ' + client.user.tag + "!");
 });
 
+const BOT = '558810306755690497';
+const JUSTIN = '188395541347041281';
+
+function checkIfBotIsSender(msg) {
+  return msg.author.id == BOT;
+}
+
 t = 0;
-prevMsg = null;
 
 client.on('message', msg => {
-  if (msg.author.id == '188395541347041281') {
+  msgText = msg.content.toLowerCase();;
+  if (msg.author.id == JUSTIN) {
     if (msg.createdTimestamp - t > 60000)
       msg.reply('SHUTUP');
     t = msg.createdTimestamp;
     return;
   }
-  if (msg.channel.id != '559310068240351233' && !(prevMsg == null) && msg.createdTimestamp - prevMsg.createdTimestamp < 10000)
-    return;
-  if (msg.content.toLowerCase().includes('is gone')) {
-    index = msg.content.toLowerCase().indexOf(' is gone');
+  if (msgText.includes('is gone')) {
+    index = msgText.indexOf(' is gone');
     while (index > 0 && msg.content.charAt(index - 1).match('[a-zA-Z ]') != null) {
       index--;
     }
@@ -28,38 +33,46 @@ client.on('message', msg => {
     name = msg.content.substring(index, msg.content.indexOf(' is gone'));
     msg.channel.send(`\:crab: ${name} is gone \:crab:`);
   }
-  if (msg.isMentioned(client.user) && msg.content.toLowerCase().includes('love')) {
+  if (msg.isMentioned(client.user) && msgText.includes('love')) {
     msg.reply('someday someone will love you too');
-  } else if (msg.isMentioned(client.user) && msg.content.toLowerCase().includes('tell me a joke')) {
+  } else if (msg.isMentioned(client.user) && msgText.includes('tell me a joke')) {
     msg.channel.send('Your life');
-  } else if (msg.content.toLowerCase().includes('nasus')) {
+  } else if ((msgText.includes('nasus')
+    || msgText.includes('stack')  
+    || msgText.includes('cane')  
+    || msgText.includes('susan'))
+    && !checkIfBotIsSender(msg)
+    ) {
     msg.channel.send('STACK!');
-  } else if ((msg.content.toLowerCase().includes('queen')
-    || msg.content.toLowerCase().includes('janet'))
-    && msg.author.id != '558810306755690497') {
+  } else if ((msgText.includes('queen')
+    || msgText.includes('janet'))
+    && !checkIfBotIsSender(msg)) {
    msg.channel.send('Janet-kun is our queen!');
-  } else if (msg.content.toLowerCase().includes('peel')
-    && msg.author.id != '558810306755690497') {
+  } else if (msgText.includes('peel')
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send('Peel David!');
-  } else if (msg.content.toLowerCase().includes('best girl')
-    && msg.author.id != '558810306755690497') {
+  } else if (msgText.includes('best girl')
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send('Best girl = Janet <3');
-  } else if (msg.content.toLowerCase().includes('abi')) {
+  } else if (msgText.includes('abi')
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send('Who is best girl\'s best girl? ABI');
-  } else if (msg.content.toLowerCase() == 'sneeze') {
+  } else if (msgText == 'sneeze') {
     msg.channel.send('ACHOO');
-  } else if (msg.content.toLowerCase().includes('ez')) {
+  } else if (msgText.includes('ez')
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send('love is ez but i am bz');
-  } else if (msg.content.toLowerCase() == 'jutsu') {
+  } else if (msgText == 'jutsu'
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send(jutsu.pickJutsu() + '!');
-  } else if (msg.content.toLowerCase().includes('sadboi hours')) {
+  } else if (msgText.includes('sadboi hours')) {
     msg.reply('its okay to be alone forever');
-  } else if (msg.content.toLowerCase().includes('friend')
-     && msg.author.id != '558810306755690497') {
+  } else if (msgText.includes('friend')
+    && !checkIfBotIsSender(msg)) {
     msg.channel.send('friends...what are those?');
   }
   prevMsg = msg;
 });
 
-client.login('NTU4ODEwMzA2NzU1NjkwNDk3.D3g8Fw.VT78BzdeT9EvKsdEpUi2H1SkE9M');
+client.login(process.env.TOKEN);
 
