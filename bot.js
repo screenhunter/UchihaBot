7b0e120f63c086b90827dbd3f5e06a1e4334de8d
Rajat <khanna.rjt@gmail.com>
2019-03-30 04:30:46 +0000

    make bot less trigger happy

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
+const schedule = require('node-schedule');
 const Discord = require('discord.js');
-const jutsu = require('./naruto.js');
+const naruto = require('./naruto.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -10,18 +11,43 @@ const BOT = '558810306755690497';
 const JUSTIN = '188395541347041281';
 const JANET = '158438213273255936';
 
+const STREAM = '536340953892651058';
+
 function checkIfBotIsSender(msg) {
   return msg.author.id == BOT;
 }
 
+function checkIfTriggerNameCommand(msg) {
+  return Math.random() < 0.1 || msg.isMentioned(client.user);
+}
+
+job = schedule.scheduleJob('0 16 * * *', function() {
+  client.channels.get('517137845589245974').send(naruto.pickNinja());
+});
+
+client.on('presenceUpdate', (oldMember, newMember) => {
+  if (newMember.presence.game != null && newMember.presence.game.streaming
+      && (oldMember.presence.game == null || !oldMember.presence.game.streaming)) {
+    client.channels.get(STREAM).send(newMember.displayName + " is streaming " + newMember.presence.game.name + "!\nCheck them out at " + newMember.presence.game.url);
+  }
+});
+
 client.on('message', msg => {
+  
+  msgText = msg.content.toLowerCase();
   if (checkIfBotIsSender(msg))
     return;
   
-  msgText = msg.content.toLowerCase();;
-  
-  if (msg.author.id == JUSTIN && Math.random() < .2) {
+  if(checkIfTriggerNameCommand(msg)) {
+    if (msg.author.id == JUSTIN) {
       msg.reply('SHUTUP');
+    } else if (msgText.includes('queen') || msgText.includes('janet')) {
+      msg.channel.send('Janet-kun is our precious queen!');
+    } else if (msgText.includes('best girl')) {
+      msg.channel.send('Best girl = Janet <3');
+    } else if (msgText.includes('abi')) {
+      msg.channel.send('goose\'s master');
+    }
     return;
   }
   
@@ -37,70 +63,36 @@ client.on('message', msg => {
     return;
   }
   
-  if (
-    msg.isMentioned(client.user)
-    && msgText.includes('love')
-  ) {
+  if (msg.isMentioned(client.user) && msgText.includes('love')) {
     msg.reply('someday someone will love you too');
-  } else if (
-    msg.isMentioned(client.user)
-    && msgText.includes('tell me a joke')
-  ) {
+  } else if (msg.isMentioned(client.user) && msgText.includes('tell me a joke')) {
     msg.channel.send('Your life');
   } else if (
     msgText.includes('nasus')
-    || msgText.includes('stack')  
-    || msgText.includes('cane')  
+    || msgText.includes('stack')
+    || msgText.includes('cane')
     || msgText.includes('susan')
   ) {
     msg.channel.send('STACK!');
-  } else if (
-    msgText.includes('queen')
-    || msgText.includes('janet')
-  ) {
-    msg.channel.send('Janet-kun is our precious queen!');
-  } else if (
-    msgText.includes('peel')
-  ) {
+  } else if (msgText.includes('peel')) {
     msg.channel.send('Peel David!');
-  } else if (
-    msgText.includes('best girl')
-  ) {
-    msg.channel.send('Best girl = Janet <3');
-  } else if (
-    msgText.includes('abi')
-  ) {
-    msg.channel.send('goose\'s e-gril');
-  } else if (
-    msgText == 'sneeze'
-  ) {
+  } else if (msgText == 'sneeze') {
     msg.channel.send('ACHOO');
-  } else if (
-    msgText.includes('ez')
-  ) {
+  } else if (msgText.includes('anthem')) {
     msg.channel.send('love is ez but i am bz');
-  } else if (
-    msgText.includes('jutsu')
-  ) {
-    msg.channel.send(jutsu.pickJutsu() + '!');
-  } else if (
-    msgText.includes('sadboi hours')
-  ) {
-    msg.reply('its okay to be alone forever');
-  } else if (
-    msgText.includes('friend')
-  ) {
+  } else if (msgText.includes('jutsu')) {
+    msg.channel.send(naruto.pickJutsu() + '!');
+  } else if (msgText.includes('sadboi hours')) {
+    msg.reply('its okay to be alone forever...sike have fun being alone loser');
+  } else if (msgText.includes('friend')) {
     msg.channel.send('friends...what are those?');
-  } else if (
-    msgText.includes('support')
-    || msgText.includes('galio')
-  ) {
+  } else if (msgText.includes('support')|| msgText.includes('galio')) {
     j = client.users.get(JANET);
     msg.channel.send(`${j} play galio support`)
-  } else if (
-    msgText.includes('power')
-  ) {
+  } else if (msgText.includes('power')) {
     msg.channel.send('The whites dont deserve power');
+  } else if (msgText.includes('underage')) {
+    msg.channel.send('ABORT ABORT');
   }
 });
 
--- naruto.js
+++ naruto.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
-module.exports = {
-  pickJutsu: function() {
-    jutsuList = fs.readFileSync('jutsu.txt', 'utf-8').split('\n');
-    return jutsuList[Math.floor(Math.random()*jutsuList.length)];
-  }
+module.exports.pickJutsu = function() {
+  jutsuList = fs.readFileSync('jutsu.txt', 'utf-8').split('\n');
+  return jutsuList[Math.floor(Math.random()*jutsuList.length)];
+}
+
+module.exports.pickNinja = function() {
+  ninjaList = fs.readFileSync('ninja.txt', 'utf-8').split('\n');
+  return ninjaList[Math.floor(Math.random()*ninjaList.length)];
 }
